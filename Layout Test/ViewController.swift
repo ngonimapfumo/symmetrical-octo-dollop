4387ec6f0b4923d2da9fc61815799be7f04eb000
Ngoni <ngonimapfumo@hotmail.com>
2025-02-12 18:18:27 +0200

    Initial Commit

---
+++ Layout Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Layout Test
+//
+//  Created by Ngoni Mapfumo on 2/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Layout TestTests/Layout_TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  Layout_TestTests.swift
+//  Layout TestTests
+//
+//  Created by Ngoni Mapfumo on 2/12/25.
+//
+
+import Testing
+@testable import Layout_Test
+
+struct Layout_TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
