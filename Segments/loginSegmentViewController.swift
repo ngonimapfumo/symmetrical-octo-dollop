7b003cdebefdae1626a76a053937924913b9f5a4
Ngoni <ngonidzashe.mapfumo@ecocashholdings.co.zw>
2025-02-12 21:30:49 +0200

    add base files

---
+++ Segments/loginSegmentViewController.swift
@@ -0,0 +1,34 @@
+//
+//  loginSegmentViewController.swift
+//  Layout Test
+//
+//  Created by Ngoni Mapfumo on 2025/02/12.
+//
+
+import UIKit
+
+class loginSegmentViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
