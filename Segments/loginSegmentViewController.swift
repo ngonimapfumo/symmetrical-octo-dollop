4f1f0c70e3f432d67af3cfad51fb3e4b14a4cd7d
Ngoni <ngonidzashe.mapfumo@ecocashholdings.co.zw>
2025-02-13 00:31:04 +0200

    add base files

--- Segments/loginSegmentViewController.swift
+++ Segments/loginSegmentViewController.swift
@@ -13,20 +13,25 @@ class loginSegmentViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         usernameTextField.attributedPlaceholder = NSAttributedString(string:"username",
-                                                                   attributes:[NSAttributedString.Key.foregroundColor:UIColor(red: 99.0/255.0, green: 99.0/255.0, blue: 99.0/255.0, alpha: 0.6)])
+                                                                     attributes:[NSAttributedString.Key.foregroundColor:UIColor(red: 99.0/255.0, green: 99.0/255.0, blue: 99.0/255.0, alpha: 0.6)])
         
         passwordTextField.attributedPlaceholder = NSAttributedString(string:"password",
-                                                                   attributes:[NSAttributedString.Key.foregroundColor:UIColor(red: 99.0/255.0, green: 99.0/255.0, blue: 99.0/255.0, alpha: 0.6)])
+                                                                     attributes:[NSAttributedString.Key.foregroundColor:UIColor(red: 99.0/255.0, green: 99.0/255.0, blue: 99.0/255.0, alpha: 0.6)])
         
-        
+        usernameTextField.addPaddingToTextField()
         
         
     }
-
-
+}
+extension UITextField{
+    
+    func addPaddingToTextField(){
+        
+        
+    }
 }
